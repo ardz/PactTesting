7de6c0ee7fee17bb1d4e18e2ddd470e50ffd7017
Richard Peckett <richard.peckett@outlook.com>
2018-01-30 21:01:25 +0000

    initial add

---
+++ ConsumerTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsumerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            const string foo = "hello";
+
+            Console.Write(foo);
+        }
+    }
+}
